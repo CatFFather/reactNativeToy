0fce231cd5c432f3119b86458a269fbfd4a6c56d
이재홍 <jh2000lee@delphicom.net>
2023-05-09 18:33:15 +0900

    webview Test

--- App.js
+++ App.js
@@ -1,21 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>테스트ㅎㅇㅎㅇ테스트ㅎㅇㅎㅇ1</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <WebView source={{ uri: 'https://catffatheryoutube.netlify.app/' }} />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
