147460b9590f3241400770741d4ccedcb2a72f37
unknown <jh2000lee@delphicom.net>
2023-05-20 03:33:57 +0700

    [ADD]commonStyle 추가

--- src/styles/commonStyle.js
+++ src/styles/commonStyle.js
@@ -1,4 +1,33 @@
-export const basicScreenStyle = {
-  //   headerStyle: { height: 50 },
-  cardStyle: { backgroundColor: '#FFFFFF' },
+export const withHeaderStyle = {
+  cardStyle: {
+    backgroundColor: '#FFFFFF',
+  },
 };
+
+export const withoutHeaderStyle = {
+  cardStyle: {
+    backgroundColor: '#FFFFFF',
+    ...padding(40, 20, 40, 20),
+  },
+};
+
+export const screenTitle = {
+  fontSize: 22,
+  fontWeight: 700,
+  lineHeight: 33,
+};
+export const screenSubTitle = {
+  fontSize: 16,
+  fontWeight: 400,
+  lineHeight: 24,
+  color: '#999999',
+};
+
+export function padding(a, b, c, d) {
+  return {
+    paddingTop: a,
+    paddingRight: b ? b : a,
+    paddingBottom: c ? c : a,
+    paddingLeft: d ? d : b ? b : a,
+  };
+}
