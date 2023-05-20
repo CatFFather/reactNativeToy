155f00b051da52ce02cd0c2902213bb49908b596
unknown <jh2000lee@nate.com>
2023-05-20 21:09:12 +0700

    [ADD]Dot 추가

---
+++ src/components/common/Dot.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+export default function Dot() {
+  return <View style={styles.dot}></View>;
+}
+
+const styles = StyleSheet.create({
+  dot: {
+    width: 4,
+    height: 4,
+    borderWidth: 1,
+    borderRadius: '50%',
+    borderColor: '#000000',
+    backgroundColor: '#000000',
+  },
+});
