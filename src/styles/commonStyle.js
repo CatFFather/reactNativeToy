765b5dc77e7ad3fd332a1c96f2c1deb0c136c87b
이재홍 <jh2000lee@nate.com>
2023-05-22 17:25:27 +0900

    [ADD]disabled color 추가

--- src/styles/commonStyle.js
+++ src/styles/commonStyle.js
@@ -1,10 +1,17 @@
 export const colors = {
   mainBackgroundColor: '#FFFFFF',
   signatureColor: '#DB3E07',
+  blank: '#E5E5E5',
+  disabled: {
+    trueBackground: '#F5F5F5',
+    falseBackground: '#DB3E07',
+    trueText: '#BBBBBB',
+    falseText: '#FFFFFF',
+  },
 };
 
 export const withHeaderStyle = {
-  ...padding(0, 20, 0, 20),
+  ...padding(30, 20, 30, 20),
 };
 
 export const withoutHeaderStyle = {
@@ -27,6 +34,10 @@ export const align = {
   alignCenter: {
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  basicRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   rowAlignCenter: {
     flexDirection: 'row',
