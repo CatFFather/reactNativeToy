a7492ef978e650c963af6b4b20154283f0d09994
이재홍 <jh2000lee@nate.com>
2023-05-23 18:46:55 +0900

    [MOD]버그 수정

--- src/screens/setting/SettingMainScreen.jsx
+++ src/screens/setting/SettingMainScreen.jsx
@@ -20,41 +20,41 @@ import CommonBtn from '../../components/button/CommonBtn';
 
 export default function SettingMain({ navigation }) {
   const { logout, user } = useAuthStore((state) => state);
-  function test() {}
+  function test() {
+    console.log('pressss!!!');
+  }
   return (
     <SafeAreaView header={true}>
       <ScrollView>
         <View style={{ gap: 30 }}>
           {/* 이름 */}
-          <View style={screenTitle}>{user.name} 님</View>
+          <View>
+            <Text style={screenTitle}>{user.name} 님</Text>
+          </View>
           <Divider />
           {/* 정보설정 */}
           <View style={{ gap: 30 }}>
-            <View style={styles.subTitle}>정보 설정</View>
-            <Pressable style={styles.pressableWrap}>
+            <View>
+              <Text style={styles.subTitle}>정보 설정</Text>
+            </View>
+            <Pressable style={styles.pressableWrap} onPress={test}>
               <View style={styles.labelWrap}>
                 <AntDesign name="form" size={28} color="black" />
-                <Text style={styles.label} onPress={test}>
-                  내 정보 수정
-                </Text>
+                <Text style={styles.label}>내 정보 수정</Text>
               </View>
               <AntDesign name="right" size={28} color="black" />
             </Pressable>
-            <Pressable style={styles.pressableWrap}>
+            <Pressable style={styles.pressableWrap} onPress={logout}>
               <View style={styles.labelWrap}>
                 <AntDesign name="logout" size={28} color="black" />
-                <Text style={styles.label} onPress={logout}>
-                  로그아웃
-                </Text>
+                <Text style={styles.label}>로그아웃</Text>
               </View>
               <AntDesign name="right" size={28} color="black" />
             </Pressable>
-            <Pressable style={styles.pressableWrap}>
+            <Pressable style={styles.pressableWrap} onPress={test}>
               <View style={styles.labelWrap}>
                 <AntDesign name="infocirlceo" size={28} color="black" />
-                <Text style={styles.label} onPress={test}>
-                  버전 정보
-                </Text>
+                <Text style={styles.label}>버전 정보</Text>
               </View>
               <AntDesign name="right" size={28} color="black" />
             </Pressable>
@@ -62,13 +62,13 @@ export default function SettingMain({ navigation }) {
           <Divider />
           {/* 약관 */}
           <View style={{ gap: 30 }}>
-            <View style={styles.subTitle}>정보 설정</View>
-            <Pressable style={styles.pressableWrap}>
+            <View>
+              <Text style={styles.subTitle}>정보 설정</Text>
+            </View>
+            <Pressable style={styles.pressableWrap} onPress={test}>
               <View style={styles.labelWrap}>
                 <AntDesign name="filetext1" size={28} color="black" />
-                <Text style={styles.label} onPress={test}>
-                  이용 약관
-                </Text>
+                <Text style={styles.label}>이용 약관</Text>
               </View>
               <AntDesign name="right" size={28} color="black" />
             </Pressable>
