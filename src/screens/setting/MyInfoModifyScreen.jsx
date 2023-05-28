ee69f0d0748c70e5c71b418414348465ad66e8b1
unknown <jh2000lee@nate.com>
2023-05-28 11:47:22 +0700

    [MOD]내 정보 수정 화면 완료

--- src/screens/setting/MyInfoModifyScreen.jsx
+++ src/screens/setting/MyInfoModifyScreen.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import { Text, View, StyleSheet, ScrollView } from 'react-native';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 // HOOK
 import useInput from '../../hooks/useInput';
 // UTILE
-import { screenTitle, screenSubTitle, align } from '../../styles/commonStyle';
+import {
+  screenTitle,
+  screenSubTitle,
+  align,
+  commonLabel,
+} from '../../styles/commonStyle';
 // STORE
 import { useAuthStore } from '../../stores/AuthStore';
 // COMPONENT
 import SafeAreaView from '../../components/layout/SafeAreaView';
 import Divider from '../../components/common/Divider';
-import SettingMenuItem from '../../components/container/setting/SettingMenuItem';
+import CommonBtn from '../../components/button/CommonBtn';
 import LabelInput from '../../components/input/LabelInput';
 
 export default function SettingMain({ navigation }) {
@@ -17,23 +23,89 @@ export default function SettingMain({ navigation }) {
   const name = useInput({ placeholder: '홍길동', initValue: user.name });
   return (
     <SafeAreaView header={true}>
-      <View style={{ gap: 30 }}>
-        {/* 이름 */}
-        <View>
-          <Text style={screenTitle}>안녕하세요!</Text>
+      <View style={{ flex: 1, gap: 60 }}>
+        <View style={styles.nameWrap}>
+          <View>
+            <Text style={screenSubTitle}>안녕하세요!</Text>
+          </View>
+          <View>
+            <Text style={styles.name}>{user.id}</Text>
+            <Divider style={styles.nameDivider} />
+          </View>
         </View>
-        <View>
-          <Text style={screenTitle}>{user.id} 님</Text>
+        <View style={{ gap: 30 }}>
+          <View>
+            <View>
+              <Text style={commonLabel}>업체명</Text>
+            </View>
+            <View>
+              <Text style={styles.dataValue}>일이삼 공업사</Text>
+            </View>
+          </View>
+          <LabelInput labelProps={{ name: '이름' }} inputProps={{ ...name }} />
+          <View style={styles.phoneWrap}>
+            <View>
+              <Text style={styles.dataName}>휴대폰 번호</Text>
+            </View>
+            <View style={{ ...align.basicRow, gap: 10 }}>
+              <Text style={styles.dataValue}>01022221111</Text>
+              <Button title="번호변경" style={styles.phoneBtn} />
+            </View>
+          </View>
+          <View style={styles.pwWrap}>
+            <View>
+              <Text style={styles.dataName}>비밀번호 변경</Text>
+            </View>
+            <View>
+              <AntDesign name="right" size={28} color="black" />
+            </View>
+          </View>
         </View>
-        <LabelInput labelProps={{ name: '이름' }} inputProps={{ ...name }} />
+      </View>
+      <View>
+        <CommonBtn
+          // disabled={!isChecked['ALL']}
+          label="저장"
+          // onPress={() => test('TERM')}
+        />
       </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  subTitle: {
-    ...screenSubTitle,
-    fontSize: 12,
+  nameWrap: {
+    ...align.alignCenter,
+    gap: 5,
+  },
+  name: {
+    ...screenTitle,
+    zIndex: 5,
+  },
+  nameDivider: {
+    borderBottomWidth: 8,
+    borderColor: '#DB3E07',
+    opacity: 0.4,
+    marginTop: -8,
+  },
+  dataName: {
+    fontSize: 16,
+    color: '#000000',
+  },
+  dataValue: {
+    fontSize: 16,
+    color: '#BBBBBB',
+  },
+  phoneWrap: {
+    ...align.basicRow,
+    justifyContent: 'space-between',
+  },
+  phoneBtn: {
+    backgroundColor: '#FFFFFF',
+    color: '#000000',
+  },
+  pwWrap: {
+    ...align.basicRow,
+    justifyContent: 'space-between',
   },
 });
