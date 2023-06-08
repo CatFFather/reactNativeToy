a8af5c2cca45817a71fc601bf577e07e68012dde
이재홍 <jh2000lee@nate.com>
2023-06-08 18:31:55 +0900

    [MOD]KeyboardAvoidingView 적용진행중...

--- src/screens/setting/MyInfoModifyScreen.jsx
+++ src/screens/setting/MyInfoModifyScreen.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+} from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 // HOOK
 import useInput from '../../hooks/useInput';
@@ -22,54 +30,119 @@ export default function SettingMain({ navigation }) {
   const { user } = useAuthStore((state) => state);
   const name = useInput({ placeholder: '홍길동', initValue: user.name });
   return (
-    <SafeAreaView header={true}>
-      <View style={{ flex: 1, gap: 60 }}>
-        <View style={styles.nameWrap}>
-          <View>
-            <Text style={screenSubTitle}>안녕하세요!</Text>
+    <KeyboardAvoidingView
+      //  keyboardVerticalOffset={Platform.OS === 'ios' ? 44 + statusBarHeight : aosOffset}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={{ flex: 1 }}
+    >
+      <SafeAreaView header={true}>
+        <ScrollView style={{ flex: 1, gap: 60 }}>
+          <View style={styles.nameWrap}>
+            <View>
+              <Text style={screenSubTitle}>안녕하세요!</Text>
+            </View>
+            <View>
+              <Text style={styles.name}>{user.id}</Text>
+              <Divider style={styles.nameDivider} />
+            </View>
           </View>
-          <View>
-            <Text style={styles.name}>{user.id}</Text>
-            <Divider style={styles.nameDivider} />
+          <View style={{ gap: 30 }}>
+            <View>
+              <View>
+                <Text style={commonLabel}>업체명</Text>
+              </View>
+              <View>
+                <Text style={styles.dataValue}>일이삼 공업사</Text>
+              </View>
+            </View>
+            <LabelInput
+              labelProps={{ name: '이름' }}
+              inputProps={{ ...name }}
+            />
+            <View style={styles.phoneWrap}>
+              <View>
+                <Text style={styles.dataName}>휴대폰 번호</Text>
+              </View>
+              <View style={{ ...align.basicRow, gap: 10 }}>
+                <Text style={styles.dataValue}>01022221111</Text>
+                <Button title="번호변경" style={styles.phoneBtn} />
+              </View>
+            </View>
+            <View style={styles.pwWrap}>
+              <View>
+                <Text style={styles.dataName}>비밀번호 변경</Text>
+              </View>
+              <View>
+                <AntDesign name="right" size={28} color="black" />
+              </View>
+            </View>
+            <View style={styles.pwWrap}>
+              <View>
+                <Text style={styles.dataName}>비밀번호 변경</Text>
+              </View>
+              <View>
+                <AntDesign name="right" size={28} color="black" />
+              </View>
+            </View>
+            <View style={styles.pwWrap}>
+              <View>
+                <Text style={styles.dataName}>비밀번호 변경</Text>
+              </View>
+              <View>
+                <AntDesign name="right" size={28} color="black" />
+              </View>
+            </View>
+            <View style={styles.pwWrap}>
+              <View>
+                <Text style={styles.dataName}>비밀번호 변경</Text>
+              </View>
+              <View>
+                <AntDesign name="right" size={28} color="black" />
+              </View>
+            </View>
+            <View style={styles.pwWrap}>
+              <View>
+                <Text style={styles.dataName}>비밀번호 변경</Text>
+              </View>
+              <View>
+                <AntDesign name="right" size={28} color="black" />
+              </View>
+            </View>
+            <View style={styles.pwWrap}>
+              <View>
+                <Text style={styles.dataName}>비밀번호 변경</Text>
+              </View>
+              <View>
+                <AntDesign name="right" size={28} color="black" />
+              </View>
+            </View>
+            <View style={styles.pwWrap}>
+              <View>
+                <Text style={styles.dataName}>비밀번호 변경</Text>
+              </View>
+              <View>
+                <AntDesign name="right" size={28} color="black" />
+              </View>
+            </View>
+            <View style={styles.pwWrap}>
+              <View>
+                <Text style={styles.dataName}>비밀번호 변경</Text>
+              </View>
+              <View>
+                <AntDesign name="right" size={28} color="black" />
+              </View>
+            </View>
           </View>
+        </ScrollView>
+        <View>
+          <CommonBtn
+            // disabled={!isChecked['ALL']}
+            label="저장"
+            // onPress={() => test('TERM')}
+          />
         </View>
-        <View style={{ gap: 30 }}>
-          <View>
-            <View>
-              <Text style={commonLabel}>업체명</Text>
-            </View>
-            <View>
-              <Text style={styles.dataValue}>일이삼 공업사</Text>
-            </View>
-          </View>
-          <LabelInput labelProps={{ name: '이름' }} inputProps={{ ...name }} />
-          <View style={styles.phoneWrap}>
-            <View>
-              <Text style={styles.dataName}>휴대폰 번호</Text>
-            </View>
-            <View style={{ ...align.basicRow, gap: 10 }}>
-              <Text style={styles.dataValue}>01022221111</Text>
-              <Button title="번호변경" style={styles.phoneBtn} />
-            </View>
-          </View>
-          <View style={styles.pwWrap}>
-            <View>
-              <Text style={styles.dataName}>비밀번호 변경</Text>
-            </View>
-            <View>
-              <AntDesign name="right" size={28} color="black" />
-            </View>
-          </View>
-        </View>
-      </View>
-      <View>
-        <CommonBtn
-          // disabled={!isChecked['ALL']}
-          label="저장"
-          // onPress={() => test('TERM')}
-        />
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+    </KeyboardAvoidingView>
   );
 }
 
