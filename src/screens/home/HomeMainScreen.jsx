16555cd7eeb0bb47e06afb8690c40d22f8b16eb1
이재홍 <jh2000lee@nate.com>
2023-06-12 18:41:30 +0900

    [MOD]이미지 선택기 사용 공부중...

--- src/screens/home/HomeMainScreen.jsx
+++ src/screens/home/HomeMainScreen.jsx
@@ -1,18 +1,48 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Image } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import * as ImagePicker from 'expo-image-picker';
 // UTILE
 import { withoutHeaderStyle } from '../../styles/commonStyle';
 // STORE
 import { useAuthStore } from '../../stores/AuthStore';
 // COMPONENT
 import ScrollScreenContainer from '../../components/layout/ScrollScreenContainer';
+import ImageViewer from '../../components/img/ImageViewer';
+import Button from '../../components/button/Button';
+
+const PlaceholderImage = require('../../assets/images/background-image.png');
 
 export default function HomeMainScreen({ navigation }) {
   const { user } = useAuthStore((state) => state);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
   return (
     <ScrollScreenContainer isHeader={false}>
       <View style={styles.container}>
-        <Text>home!!!!</Text>
+        <View style={styles.imageContainer}>
+          <ImageViewer placeholderImageSource={PlaceholderImage} />
+        </View>
+        <View style={styles.footerContainer}>
+          <Button
+            theme="primary"
+            label="Choose a photo"
+            onPress={pickImageAsync}
+          />
+          <Button label="Use this photo" />
+        </View>
+        <StatusBar style="auto" />
       </View>
     </ScrollScreenContainer>
   );
@@ -23,5 +53,15 @@ const styles = StyleSheet.create({
     ...withoutHeaderStyle,
     gap: 30,
     flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
